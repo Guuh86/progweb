21d310b516da3fc02abaf16424fcee644ef3372f
Luiz Gustavo <42076223+Guuh86@users.noreply.github.com>
2018-10-10 19:07:09 -0300

    Add files via upload

--- voto.php
+++ voto.php
@@ -1,15 +1,18 @@
 <?php
 session_start();
-
 $btn = @$_POST['btn'];
+
 $p1 = 0;
 $p2 = 0;
 
+$_SESSION['p1'];
+$_SESSION['p2'];
+
 if ($btn == '1') {
-	$_SESSION['p1'] += 1;
-}elseif ($btn == '2') {
-	$_SESSION['p2'] += 1;
-}elseif ($btn == '3') {
+	$_SESSION['p1'] = $_SESSION['p1'] + 1;
+}else if($btn == '2'){
+	$_SESSION['p2'] = $_SESSION['p2'] + 1;
+}else if($btn == '3'){
 	$_SESSION['p1'] = 0;
 	$_SESSION['p2'] = 0;
 }
@@ -20,28 +23,30 @@ if ($btn == '1') {
 	<title>Votação</title>
 </head>
 <body>
-	<center><h2>SisVoto</h2></center>
+     <font face="Arial">
+	<center><h2><font color="green">Sis</font><font color="yellow">Voto</font></h2></center>
 	<hr>
 	<form method="post">
 	<p>Partido 1 <button value="1" name="btn">Votar</button></p>
 	<p>Partido 2 <button value="2" name="btn">Votar</button></p>
-	</form>
 	<hr>
 	<p>Resultado:</p>
-	<p>Partido 1 <?php echo $_SESSION['p1'] ?> Votos</p>
-	<p>Partido 2 <?php echo $_SESSION['p2'] ?> Votos</p>
+	<p><?php echo "Partido 1 ".$_SESSION['p1']." Votos"; ?></p>
+	<p><?php echo "Partido 1 ".$_SESSION['p2']." Votos"; ?></p>
 	<div>
 		<button value="3" name="btn">Limpar</button>
 	</div>
+	</form>
+	</font>
 
 	<style type="text/css">
 		body{
 			margin-left: 20%;
-			margin-right: 20%; 
+			margin-right: 20%;
+			background-color: lightblue; 
 		}
 		hr{
-			background-color: silver;
-			color: silver;
+			border-color: silver;
 		}
 		div{
 			margin-left: 150px;
