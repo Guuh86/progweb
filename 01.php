0acb23499afdbb1278cc34c501f4ebb7396c3105
Luiz Gustavo <42076223+Guuh86@users.noreply.github.com>
2018-10-11 15:20:39 -0300

    Add files via upload

---
+++ 01.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Questão 01</title>
+</head>
+<body>
+	<form method="POST">
+		Digite um número entre 2 e 20: <input type="text" name="numero">
+		<button>ENVIAR</button><br/>
+	</form>
+
+<?php
+
+$n = @$_POST['numero'];
+
+for($c = 1; $c <= $n; $c++){
+	for($y = 1; $y <= $c; $y++){
+		echo "$y"."<br/>";
+	}
+}
+
+
+for($c = $c-2; $c > 0; $c--){
+	for($y = 1; $y <= $c; $y++){
+		echo "$y"."<br/>";
+	}
+}
+
+?>
+
+	
+</body>
+</html>
